fe87f601ee60c2aa4f66d29ecefe32401a4ef2ef
Michael Congrove <congrove91@gmail.com>
2020-10-30 10:22:57 -0400

    Push of the correct file with comments to explain

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
-//Kata 1
+//Kata 1- Using A=2 and B=4
 function add(a, b) {
   return a + b;
 }
+// returns the answer of 6, simple addition of 2+4
 
-//Kata 2
+//Kata 2- Using A=6 and B=4
 function multiply(a, b) {
   let count = 0;
   let result = 0;
@@ -13,8 +14,9 @@ function multiply(a, b) {
   }
   return result;
 }
+// returns answer of 24, runs until count is less than 4 and then adds once more
 
-//Kata 3
+//Kata 3 Using X=2 and N=8
 function power(n, x) {
   let count = 0;
   let result = 1;
@@ -24,8 +26,9 @@ function power(n, x) {
   }
   return result;
 }
+// returns answer of 256, runs until count count hits 7 then adds once more
 
-//Kata4
+//Kata4 Using A=5
 function factorial(a) {
   let count = 1;
   let result = 1;
@@ -35,3 +38,4 @@ function factorial(a) {
   }
   return result;
 }
+//returns answer of 120, runs until count is less than or equal to one, decreases each time
